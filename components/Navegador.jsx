201ecd386ed1702eb28d2868c18c778fa21e5ba6
Gerard Siles <gerardsiles@gmail.com>
2022-10-15 14:54:10 +0000

    V1.1 Menu con textos interactivos

--- components/Navegador.jsx
+++ components/Navegador.jsx
@@ -1,19 +1,48 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Alert, TouchableOpacity } from 'react-native';
 import { useTailwind } from 'tailwind-rn/dist';
 const Navegador = () => {
 	const tw = useTailwind();
+	const onEvolucion = () => Alert.alert('Evolucion apretado');
+	const onNuevoReto = () => Alert.alert('Nuevo Reto apretado');
+	const onPerfil = () => Alert.alert('Perfil apretado');
+	const onContactar = () => Alert.alert('Contactar apretado');
+	const onCompletados = () => Alert.alert('Retos Completados apretado');
+	const onActivos = () => Alert.alert('Retos Activos apretado');
 	return (
 		<View style={styles.container}>
-			<View>
+			<TouchableOpacity onPress={onEvolucion}>
 				<Text style={styles.text}>Evolucion</Text>
-			</View>
-			<View>
-				<Text style={styles.text}>Nuevo Reto</Text>
-			</View>
-			<Text style={styles.text}>Perfil</Text>
-			<Text style={styles.text}>Contactar</Text>
-			<Text style={styles.text}>Retos completados</Text>
-			<Text style={styles.text}>Retos Activos</Text>
+			</TouchableOpacity>
+
+			<TouchableOpacity onPress={onNuevoReto}>
+				<View>
+					<Text style={styles.text}>Nuevo Reto</Text>
+				</View>
+			</TouchableOpacity>
+
+			<TouchableOpacity onPress={onPerfil}>
+				<View>
+					<Text style={styles.text}>Perfil</Text>
+				</View>
+			</TouchableOpacity>
+
+			<TouchableOpacity onPress={onContactar}>
+				<View>
+					<Text style={styles.text}>Contactar</Text>
+				</View>
+			</TouchableOpacity>
+
+			<TouchableOpacity onPress={onCompletados}>
+				<View>
+					<Text style={styles.text}>Retos completados</Text>
+				</View>
+			</TouchableOpacity>
+
+			<TouchableOpacity onPress={onActivos}>
+				<View>
+					<Text style={styles.text}>Retos Activos</Text>
+				</View>
+			</TouchableOpacity>
 		</View>
 	);
 };
