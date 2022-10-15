099f28696ef6c26b16786a878eb468fe67001895
Gerard Siles <gerardsiles@gmail.com>
2022-10-15 13:41:20 +0000

    primer commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { TailwindProvider, useTailwind } from 'tailwind-rn';
+import Inicio from './components/Inicio';
+import Navegador from './components/Navegador';
+
+import utilities from './tailwind.json';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const tw = useTailwind();
+	return (
+		<TailwindProvider utilities={utilities}>
+			<View style={{ flex: 1 }}>
+				<ImageBackground
+					source={require('./assets/mobile-background.jpg')}
+					resizeMode='cover'
+					style={{ flex: 1, justifyContent: 'center' }}
+				>
+					<Navegador />
+				</ImageBackground>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+				<StatusBar style='auto' />
+			</View>
+		</TailwindProvider>
+	);
+}
