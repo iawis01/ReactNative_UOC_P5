7cc461d1d13065c17a3c00afec2b2f7c92bbff8f
iago0 <posseiago@gmail.com>
2022-12-08 00:06:55 +0100

    validaciones campos requeridos e int aún en pruba

--- screens/NuevoReto.jsx
+++ screens/NuevoReto.jsx
@@ -13,8 +13,9 @@ import { useLayoutEffect, useState } from "react";
 import { Image } from "@rneui/themed";
 import { AntDesign } from "@expo/vector-icons";
 import { StyleSheet } from "react-native";
-import { addDoc, collection, doc, setDoc } from "firebase/firestore"; 
+import { addDoc, collection } from "firebase/firestore"; 
 import { db } from "../db/firebaseConfig";
+
 
 const NuevoReto = () => {
   const tw = useTailwind();
@@ -31,23 +32,71 @@ const NuevoReto = () => {
 	const [prioridad, setPrioridad] = useState('');
 	const [tiempo, setTiempo] = useState('');
 
-  function addGoal() {
-		addDoc(collection(db, "retos"), {
-			id: id,
-			activo: activo,
-			categoria: categoria,
-			completado: completado,
-			detalle: detalle,
-			nombre: nombre,
-			periodicidad: periodicidad,
-			prioridad: prioridad,
-			tiempo: tiempo,
-		}).then(() => {
-			console.log('goal submitted')
-		}).catch((error) =>{
-			console.log(error);
-		});
+	const [reqErr, setReqErr] = useState(true);
+   const [intErr, setIntErr] = useState(true);
+
+	const nombreError = nombre ? '' : 'Nombre no puede estar vacio';
+
+	const messages = {
+		req: "Todos los campos son obligatorios",
+		int: "Debes introducir un número"
+	 };
+
+
+	 const reqRegex = new RegExp(/^[^]+$/);
+	 const intRegex = new RegExp(/^[0-9]+$/);
+
+	 const validateReqCategoria = () => {
+		if (reqRegex.test(categoria)) {
+			 setReqErr(false);
+		}
+		//console.log(reqErr);
+ };
+
+ const validateIntTiempo = () => {
+	if (intRegex.test(tiempo)) {
+		 setIntErr(false);
+		 
 	}
+};
+
+
+  function addGoal() {
+		setReqErr(true);
+		setIntErr(true);
+		validateReqCategoria();
+		validateIntTiempo();
+		if(reqErr){
+			alert("El campo categoria no puede estar vacío");
+		}else if(/*categoria === '' ||*/ detalle === '' || nombre === ''|| prioridad === '' || tiempo === ''|| periodicidad === ''){
+			alert(messages.req);
+		}else if(intErr){
+			alert("El campo tiempo tiene que ser un número");
+		}else{
+			addDoc(collection(db, "retos"), {
+				//TODO last id + 1
+				id: 31,
+				activo: true,
+				categoria: categoria,
+				completado: 0,
+				detalle: detalle,
+				nombre: nombre,
+				periodicidad: periodicidad,
+				prioridad: prioridad,
+				tiempo: Number(tiempo),
+			}).then(() => {
+				console.log('goal submitted')
+			}).catch((error) =>{
+				console.log(error);
+			});
+			navigation.navigate('Evolucion');
+			alert('Reto creado');
+		}
+
+
+		
+	}
+
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -87,24 +136,9 @@ const NuevoReto = () => {
         PlaceholderContent={<ActivityIndicator />}
       />
       {/*input, icon y button*/}
-			<TextInput
-        value={id} onChangeText={(id => {setId(id)})}
-        placeholder="id"
-        style={styles.inputBox}
-      ></TextInput>
-      <TextInput
-        value={activo} onChangeText={(activo => {setActivo(activo)})}
-        placeholder="activo"
-        style={styles.inputBox}
-      ></TextInput>
       <TextInput
         value={categoria} onChangeText={(categoria => {setCategoria(categoria)})}
         placeholder="categoria"
-        style={styles.inputBox}
-      ></TextInput>
-      <TextInput
-       value={completado} onChangeText={(completado => {setCompletado(completado)})}
-        placeholder="completado"
         style={styles.inputBox}
       ></TextInput>
       <TextInput
@@ -115,10 +149,11 @@ const NuevoReto = () => {
       <TextInput
        value={nombre} onChangeText={(nombre => {setNombre(nombre)})}
         placeholder="nombre"
+				patterns
         style={styles.inputBox}
       ></TextInput>
       <TextInput
-        value={periodicidad} onChangeText={(periodicidad => {setPeriodicidad(periodicidad)})}
+        value={periodicidad} keyboardType="numeric" onChangeText={(periodicidad => {setPeriodicidad(periodicidad)})}
         placeholder="periodicidad"
         style={styles.inputBox}
       ></TextInput>
@@ -128,15 +163,18 @@ const NuevoReto = () => {
         style={styles.inputBox}
       ></TextInput>
       <TextInput
-        value={tiempo} onChangeText={(tiempo => {setTiempo(tiempo)})}
+        value={tiempo}  keyboardType="numeric" onChangeText={(tiempo => {setTiempo(tiempo)})}
         placeholder="tiempo"
         style={styles.inputBox}
+				
       ></TextInput>
 
       <TouchableOpacity style={styles.buttonStyle}>
         <Button title="Crear reto" onPress={() => addGoal()} />
       </TouchableOpacity>
+
     </ScrollView>
+		
   );
 };
 
